df88989f8cb80a6eb612f9efe5199e8ad9b1e211
lwq6218 <lwq6218@gmail.com>
2024-05-27 10:44:29 +0800

    add navbar

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,20 +1,41 @@
 import React from "react";
 import type { Metadata } from "next";
+// eslint-disable-next-line camelcase
+import { Inter, Space_Grotesk } from "next/font/google";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
+import ThemeProvider from "@/context/ThemeProvider";
 
-export const metadata: Metadata = {};
-
+export const metadata: Metadata = {
+  title: "Devflow",
+  description:
+    "A community-driven platform for asking and answering programming questions. Get help, share knowledge, and collaborate with developers from around the world. Explore topics in web development, mobile app development, algorithms, data structures, and more.",
+  icons: {
+    icon: "../public/assets/images/site-logo.svg",
+  },
+};
+const inter = Inter({
+  subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+  variable: "--font-inter",
+});
+const spaceGrotesk = Space_Grotesk({
+  subsets: ["latin"],
+  weight: ["300", "400", "500", "600", "700"],
+  variable: "--font-spacegrotesk",
+});
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
-    <ClerkProvider>
-      <html lang="en">
-        <body>{children}</body>
-      </html>
-    </ClerkProvider>
+    <html lang="en">
+      <body className={`${inter.variable} ${spaceGrotesk.variable}`}>
+        <ClerkProvider>
+          <ThemeProvider>{children}</ThemeProvider>
+        </ClerkProvider>
+      </body>
+    </html>
   );
 }
---
+++ components/shared/navbar/Theme.tsx
@@ -0,0 +1,76 @@
+"use client";
+import { useTheme } from "@/context/ThemeProvider";
+import React from "react";
+import {
+  Menubar,
+  MenubarContent,
+  MenubarItem,
+  MenubarMenu,
+  MenubarTrigger,
+} from "@/components/ui/menubar";
+import Image from "next/image";
+import { themes } from "@/constants";
+
+export default function Theme() {
+  const { mode, setMode } = useTheme();
+  return (
+    <Menubar className="relative border-none bg-transparent shadow-none">
+      <MenubarMenu>
+        <MenubarTrigger className=" focus:bg-light-900 data-[state=open]:bg-light-900 dark:focus:bg-dark-200 dark:data-[state=open]:bg-dark-200">
+          {mode === "light" ? (
+            <Image
+              src="/assets/icons/sun.svg"
+              className="active-theme"
+              width={20}
+              height={20}
+              alt="light mode"
+            />
+          ) : (
+            <Image
+              className="active-theme"
+              src="/assets/icons/moon.svg"
+              width={20}
+              height={20}
+              alt="dark mode"
+            />
+          )}
+        </MenubarTrigger>
+        <MenubarContent className="absolute -right-12 mt-3 min-w-[120px] rounded border py-2 dark:border-dark-400 dark:bg-dark-200">
+          {themes.map((theme) => {
+            return (
+              <MenubarItem
+                className="flex items-center gap-4 px-2.5 py-2"
+                key={theme.value}
+                onClick={() => {
+                  setMode(theme.value);
+                  if (theme.value !== "system") {
+                    localStorage.theme = theme.value;
+                  } else {
+                    localStorage.removeItem("theme");
+                  }
+                }}
+              >
+                <Image
+                  src={theme.icon}
+                  className={`${mode === theme.value && "active-theme"}`}
+                  width={16}
+                  height={16}
+                  alt={theme.labbel}
+                />
+                <p
+                  className={`body-semibold text-light-500 ${
+                    theme.value === mode
+                      ? "text-primary-500"
+                      : "text-dark100_light900"
+                  }`}
+                >
+                  {theme.labbel}
+                </p>
+              </MenubarItem>
+            );
+          })}
+        </MenubarContent>
+      </MenubarMenu>
+    </Menubar>
+  );
+}
